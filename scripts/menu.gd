0b881f3fc893b57616291e5c35b058f930e12fb9
Vinícius Gabriel Inacio <inacioviniciusgabriel@gmail.com>
2025-08-07 15:47:11 -0300

    Scene de transition feita

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,4 +1,8 @@
 extends Control
 
+@onready var animation_player = $AnimationPlayer
+
 func _on_start_button_pressed() -> void:
+	TransitionScreen.transition()
+	await TransitionScreen.on_transition_finished
 	get_tree().change_scene_to_file("res://scenes/day_1.tscn")
