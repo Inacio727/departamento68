ab06874f6a67d702d488378541f2f44c4309d52e
Vinícius Gabriel Inacio <inacioviniciusgabriel@gmail.com>
2025-08-13 15:00:14 -0300

    DialogBasics4

--- scripts/dialog_player.gd
+++ scripts/dialog_player.gd
@@ -2,23 +2,39 @@ extends CanvasLayer
 
 @export_file("*json") var scene_text_file: String
 
-var scene_text = {}
-var selected_text = []
-var in_progress = false
+var scene_text: Dictionary = {}
+var selected_text: Array = []
+var in_progress: bool = false
 
 @onready var background = $Background
 @onready var text_label = $TextLabel
 
-func _ready() -> void:
+func _ready():
 	background.visible = false
 	scene_text = load_scene_text()
-	SignalBus.connect("display_dialog", self, "on_display_dialog")
+	SignalBus.connect("display_dialog", Callable(self, "on_display_dialog"))
 
 func load_scene_text():
-	var file = FileAccess.new()
-	if file.file_exists(scene_text_file):
-		file.open(scene_text_file, FileAccess.READ)
-		return parse_json(file.get_as_text())
+	if FileAccess.file_exists(scene_text_file):
+		var file = FileAccess.open(scene_text_file, FileAccess.READ)
+		var test_json_conv = JSON.new()
+		test_json_conv.parse(file.get_as_text())
+		return test_json_conv.get_data()
+
+func show_text():
+	text_label.text = selected_text.pop_front()
+
+func next_line():
+	if selected_text.size() > 0:
+		show_text()
+	else:
+		finish()
+
+func finish():
+	text_label.text = ""
+	background.visible = false
+	in_progress = false
+	get_tree().paused = false
 
 func on_display_dialog():
 	if in_progress:
